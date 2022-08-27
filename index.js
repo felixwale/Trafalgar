1f48ab8aeb3a497e7b43afb6d1992219848339a5
Tijani Atanda <felixwale199@gmail.com>
2022-08-27 01:08:13 -0500

    I add enhancement to my button

---
+++ index.js
@@ -0,0 +1,12 @@
+// let hambuger = document.getElementById("ham-icon");
+
+// let times = document.getElementById("close");
+
+// let navBar = document.getElementById("nav-list");
+
+
+// hambuger.addEventListener('click', () => {
+
+// hambuger.style.display = 'none'
+
+// });
