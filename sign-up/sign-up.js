390c37ce4ab4299eed876e43fadffe9549f0ff6e
Tijani Atanda <felixwale199@gmail.com>
2022-09-01 00:54:03 -0500

    updating my project

--- sign-up/sign-up.js
+++ sign-up/sign-up.js
@@ -1,4 +1,4 @@
-var userName = document.getElementById('txtName');
+const userName = document.getElementById('txtName');
 
 const loginPassword = document.getElementById('loginPassword');
 
@@ -6,93 +6,103 @@ const loginButton = document.getElementById('login');
 
 const form = document.getElementById('form');
 
+const loginEmail = document.getElementById('loginEmail')
 
 
 loginButton.disabled = true;
 
+userName.addEventListener('blur', function () {
+   
+    if(userName == 0){
 
-const validateUserName = () => {
-    
-    const firstNameValue = userName.value.trim();
-    
-    if (firstNameValue == '') {
-        errorMessage(
-          firstName,
-          'First Name cannot be blank (First Name can accept any character except numbers)'
-        );
-        
-    } else if (isValidName(firstNameValue)) {
-        errorMessage(userName, 'UserName accept any character except numbers');
-    } else {
-        success(userName);
+        er
     }
-}
 
-
-const validateLoginPassword = () => {
-  const loginPasswordValue = loginPassword.value.trim()
-
-  if (loginPasswordValue == '') {
-    errorMessage(loginPassword, 'Password cannot be blank')
-  } else if (!isValidPassword(loginPasswordValue)) {
-    errorMessage(loginPassword, 'Password should be 8 characters long')
-  } else {
-      loginButton.disabled = false;
-      success(loginPassword);
-  }
-}
-
-const validateLoginEmail = () => {
-  const loginEmailValue = loginEmail.value.trim();
-
-  if (loginEmailValue == '') {
-    errorMessage(loginEmail, 'Email cannot be blank');
-  } else if (isEmail(loginEmailValue)) {
-    errorMessage(loginEmail, 'Not a valid Email');
-  } else {
-    success(loginEmail);
-  }
-}
-
-
-const errorMessage = (input, message) => {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector('small');
-  formControl.className = 'form-control error';
-  small.innerText = message;
-}
-
-const success = (input) => {
-  const formControl = input.parentElement
-  formControl.className = 'form-control success';
-}
-
-const isEmail = (loginEmail) => {
-    mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
-    return loginEmail.match(mailformat);
-
-}
-
-const isValidPassword = (password) => {
-  if (password.length < 8) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-
-let emailValidation = loginEmail.addEventListener('blur', (e) => {
-  e.preventDefault();
-  validateLoginEmail();
 })
 
 
-let passwordValidation = loginPassword.addEventListener('blur', (e) => {
-  e.preventDefault();
-  validateLoginPassword();
-})
+// const validateUserName = () => {
+    
+//     const firstNameValue = userName.value.trim();
+    
+//     if (firstNameValue == '') {
+//         errorMessage(
+//           firstName,
+//           'First Name cannot be blank (First Name can accept any character except numbers)'
+//         );
+        
+//     } else if (isValidName(firstNameValue)) {
+//         errorMessage(userName, 'UserName accept any character except numbers');
+//     } else {
+//         success(userName);
+//     }
+// }
 
-if (emailValidation && passwordValidation) {
-  loginButton.disabled = false;
-}
+
+// const validateLoginPassword = () => {
+//   const loginPasswordValue = loginPassword.value.trim()
+
+//   if (loginPasswordValue == '') {
+//     errorMessage(loginPassword, 'Password cannot be blank')
+//   } else if (!isValidPassword(loginPasswordValue)) {
+//     errorMessage(loginPassword, 'Password should be 8 characters long')
+//   } else {
+//       loginButton.disabled = false;
+//       success(loginPassword);
+//   }
+// }
+
+// const validateLoginEmail = () => {
+//   const loginEmailValue = loginEmail.value.trim();
+
+//   if (loginEmailValue == '') {
+//     errorMessage(loginEmail, 'Email cannot be blank');
+//   } else if (isEmail(loginEmailValue)) {
+//     errorMessage(loginEmail, 'Not a valid Email');
+//   } else {
+//     success(loginEmail);
+//   }
+// }
+
+
+// const errorMessage = (input, message) => {
+//   const formControl = input.parentElement;
+//   const small = formControl.querySelector('small');
+//   formControl.className = 'form-control error';
+//   small.innerText = message;
+// }
+
+// const success = (input) => {
+//   const formControl = input.parentElement
+//   formControl.className = 'form-control success';
+// }
+
+// const isEmail = (loginEmail) => {
+//     mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+//     return loginEmail.match(mailformat);
+
+// }
+
+// const isValidPassword = (password) => {
+//   if (password.length < 8) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+
+
+// let emailValidation = loginEmail.addEventListener('blur', (e) => {
+//   e.preventDefault();
+//   validateLoginEmail();
+// });
+
+
+// let passwordValidation = loginPassword.addEventListener('blur', (e) => {
+//   e.preventDefault();
+//   validateLoginPassword();
+// });
+
+// if (emailValidation && passwordValidation) {
+//   loginButton.disabled = false;
+// };
